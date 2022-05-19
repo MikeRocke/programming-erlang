e55e5c20c1a79541824756dec0e75786dafb4a2c
MikeRocke <mikerocke1989@gmail.com>
2022-05-19 23:06:40 +0100

    perimeter functions added

--- src/geometry.erl
+++ src/geometry.erl
@@ -1,7 +1,14 @@
 -module(geometry).
--export([area/1]).
+-export([area/1, perimeter/1]).
+
+-define(Pi, 3.14159).
 
 area({rectangle, Width, Height}) -> Width * Height;
 area({square, Side}) -> Side * Side;
-area({circle, Radius}) -> 3.14159 * Radius * Radius;
+area({circle, Radius}) -> ?Pi * Radius * Radius;
 area({right_triangle, Base, Height}) -> (Base * Height)/2.
+
+perimeter({square, Side}) -> 4*Side;
+perimeter({rectangle, Width, Height}) -> 2*Width + 2*Height;
+perimeter({circle, Radius}) -> ?Pi * 2 * Radius;
+perimeter({right_triangle, Base, Height}) -> Base + Height + math:sqrt(Base * Base + Height * Height).
--- test/geometry_tests.erl
+++ test/geometry_tests.erl
@@ -5,3 +5,8 @@ area_square_test() -> 25 = geometry:area({square, 5}).
 area_rectangle_test() -> 12 = geometry:area({rectangle, 3, 4}).
 area_circle_test() -> 28.274309999999996 = geometry:area({circle, 3}).
 area_right_angle_triangle_test() -> 25.0 = geometry:area({right_triangle, 10, 5}).
+
+perimeter_square_test() -> 12 = geometry:perimeter({square, 3}).
+perimeter_rectangle_test() -> 20 = geometry:perimeter({rectangle, 4, 6}).
+perimeter_circle_test() -> 31.4159 = geometry:perimeter({circle, 5}).
+perimeter_right_triangle_test() -> 12.0 = geometry:perimeter({right_triangle, 3, 4}).
