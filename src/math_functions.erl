887c8050e166d750ef85d97c04a1d92b0581f2e5
MikeRocke <mikerocke1989@gmail.com>
2022-05-21 17:48:15 +0100

    Exercise 4.7 - Split function

--- src/math_functions.erl
+++ src/math_functions.erl
@@ -1,5 +1,5 @@
 -module(math_functions).
--export([even/1, odd/1, filter/2]).
+-export([even/1, odd/1, filter/2, split/1]).
 
 even(X) when X rem 2 =:= 0 -> true;
 even(_) -> false.
@@ -7,3 +7,15 @@ even(_) -> false.
 odd(X) -> not even(X).
 
 filter(F, L) -> [X || X <- L, F(X) =:= true].
+
+split(L) ->
+    even_and_odd_acc(L, [], []).
+
+even_and_odd_acc([], Even, Odd) ->
+    {lists:reverse(Even), lists:reverse(Odd)};
+
+even_and_odd_acc([H|T], Even, Odd) ->
+    case (even(H)) of
+        true -> even_and_odd_acc(T, [H | Even], Odd);
+        false -> even_and_odd_acc(T, Even, [H | Odd])
+    end.
--- test/math_functions_tests.erl
+++ test/math_functions_tests.erl
@@ -8,3 +8,6 @@ odd_valid_test() -> true = math_functions:odd(3).
 odd_invalid_test() -> false = math_functions:odd(4).
 
 filter_even_test() -> [2,4,6,8,10] = math_functions:filter(fun math_functions:even/1, lists:seq(1, 10)).
+
+split_empty_test() -> {[], []} = math_functions:split([]).
+split_test() -> {[2,4], [1,3,5]} = math_functions:split(lists:seq(1,5)).
