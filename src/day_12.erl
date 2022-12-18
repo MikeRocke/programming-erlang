f6b43b1ad4ecd88594fb09965437c347ddba8b0f
MikeRocke <mikerocke1989@gmail.com>
2022-12-18 12:53:45 +0000

    day 13 done

--- src/day_12.erl
+++ src/day_12.erl
@@ -2,7 +2,6 @@
 -export([start/0]).
 -compile(export_all).
 
-
 start() ->
     Lines = file_functions:read_lines("input.txt"),
     Grid = [parse_line(Line) || Line <- Lines],
@@ -11,40 +10,33 @@ start() ->
     AllPositions = [{R,C} || R <- lists:seq(1, NumberOfRows), C <- lists:seq(1, NumberOfColumns)],
     [StartingPosition] = lists:filter(fun(P) -> get_grid_value(Grid, P) =:= 0 end, AllPositions),
     [TargetPosition] = lists:filter(fun(P) -> get_grid_value(Grid, P) =:= 27 end, AllPositions),
-    % StartingPosition = {1,1}, %Lazy and hardcoded
-    % TargetPosition = {3,6}, %Lazy again
-    search(Grid, StartingPosition, TargetPosition, NumberOfRows, NumberOfColumns).
+    Graph = digraph:new([cyclic]),
+    [digraph:add_vertex(Graph, V) || V <- AllPositions],
+    [populate(Graph, V, Grid, NumberOfRows, NumberOfColumns) || V <- AllPositions],
+    ShortestPath = digraph:get_short_path(Graph, StartingPosition, TargetPosition),
+    Solution2 = [digraph:get_short_path(Graph, Bottom, TargetPosition) || Bottom <- lists:filter(fun(P) -> get_grid_value(Grid, P) =:= 1 end, AllPositions)],
+    digraph:delete(Graph),
+    length(ShortestPath),
+    lists:min([length(S) || S <- Solution2, S =/= false]).
+    
 
-search(Grid, StartingPosition, TargetPosition, NumberOfRows, NumberOfColumns) ->
-    ets:new(paths, [set, named_table]),
-    traverse(Grid, StartingPosition, TargetPosition, NumberOfRows, NumberOfColumns, []),
-    AllPaths = ets:tab2list(paths),
-    ets:delete(paths),
-    lists:min([StepCount || {_, StepCount} <- AllPaths]).
 
-traverse(_, Goal, Goal, _, _, Path) -> 
-    ets:insert(paths, {Path, length(Path)}),
-    Path;
-traverse(Grid, {R, C}, TargetPosition, NumberOfRows, NumberOfColumns, Path) ->
+
+populate(Graph, {R, C}, Grid, NumberOfRows, NumberOfColumns) ->
     Up = {R-1, C}, Down = {R+1, C}, Left = {R, C-1}, Right = {R, C+1},
     NextDirections = [Up, Down, Left, Right],
     Elevation = get_grid_value(Grid, {R, C}),
-    Visited = sets:from_list(Path),
-    IsValid = fun({Nr, Nc}) -> 
+    IsValid = fun({Nr, Nc}) ->
         (Nr > 0) and 
         (Nc > 0) and 
         (Nr =< NumberOfRows) and 
-        (Nc =< NumberOfColumns) and
-        (not sets:is_element({Nr, Nc}, Visited)) andalso
-        (get_grid_value(Grid, {Nr, Nc}) - Elevation =< 1)
+        (Nc =< NumberOfColumns) andalso
+        (max(min(get_grid_value(Grid, {Nr, Nc}), 26),1) - Elevation =< 1)
     end,
     ValidDirections = lists:filter(IsValid, NextDirections),
-    [traverse(Grid, D, TargetPosition, NumberOfRows, NumberOfColumns, [{R, C}] ++ Path) || D <- ValidDirections].
+    lists:foreach(fun(V2) -> digraph:add_edge(Graph, {R, C}, V2) end, ValidDirections).
 
 get_grid_value(Grid, {R, C}) -> lists:nth(C, lists:nth(R, Grid)).
-
-% Skip heuristic for now
-% heuristic({R1, C1}, {R2, C2}, StepCount) -> abs(R1 - R2) + abs(C1 - C2) + StepCount.
 
 parse_line(Line) ->
     ToNum = fun(X) ->
---
+++ src/day_13.erl
@@ -0,0 +1,55 @@
+-module(day_13).
+-export([start/0]).
+-compile(export_all).
+
+
+start() ->
+    Lines = file_functions:read_lines("input.txt"),
+    PacketLines = [string:trim(Line) || Line <- Lines, not string:is_empty(string:trim(Line))],
+    
+    {LeftIndexes, RightIndexes} = lists:partition(fun(I) -> I rem 2 == 1 end, [Index || Index <- lists:seq(1, length(PacketLines))]),
+    PacketPairs = [{parse(lists:nth(RI - 1, PacketLines)), parse(lists:nth(RI, PacketLines))} || RI <- RightIndexes],
+    SolitionOne = lists:filter(fun(I) -> 
+         {Left, Right} = lists:nth(I, PacketPairs),
+         correct_order(Left, Right)
+         end, lists:seq(1, length(PacketPairs))),
+    lists:sum(SolitionOne),
+
+    FirstDivider = [[2]],
+    SecondDivider = [[6]],
+    ToSort = [parse(PacketLine) || PacketLine <- PacketLines] ++ [FirstDivider, SecondDivider],
+    Sorted = lists:sort(fun correct_order/2, ToSort),
+    [FI, SI] = [I || {I, Packet} <- lists:enumerate(Sorted), (Packet =:= FirstDivider) or (Packet =:= SecondDivider) ],
+    io:format("~p  ~p ~n", [FI, SI]),
+    FI * SI.
+
+
+
+% Super thank you for teaching me this!
+% https://stackoverflow.com/questions/49291088/convert-a-string-of-list-to-a-list-in-erlang
+parse(Line) ->
+    {ok, Data, _} = erl_scan:string(Line),
+    {ok, Parsed} = erl_parse:parse_term(Data ++ [{dot, 1} || element(1, lists:last(Data)) =/= dot]),
+    Parsed.
+
+
+
+correct_order([L | LT], [R | RT]) when is_integer(L), is_integer(R), L < R -> true; %
+correct_order([L | LT], [R | RT]) when is_integer(L), is_integer(R), L =:= R -> correct_order(LT, RT); %
+correct_order([L | LT], [R | RT]) when is_integer(L), is_integer(R), L > R -> false; %
+
+
+% correct_order([L|LT], [R | RT]) when is_list(L); is_list(R)-> correct_order(L, R);
+
+correct_order([A|Ax], [B|Bx]) when is_list(A);  is_list(B) ->
+    case correct_order(A, B) of
+        ok    -> correct_order(Ax, Bx);
+        Other -> Other
+    end;
+
+correct_order([], []) -> ok; %
+
+correct_order(L, R) when is_list(L), is_integer(R) -> correct_order(L, [R]);%
+correct_order(L, R) when is_integer(L), is_list(R) -> correct_order([L], R); %
+correct_order([], R) when is_list(R), length(R) > 0 -> true; %
+correct_order(L, []) when is_list(L), length(L) > 0 -> false. %
